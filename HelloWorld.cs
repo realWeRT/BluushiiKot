1661d6d4561e24676bcac01d9bf7f4af4845c646
realWeRT <62915513+realWeRT@users.noreply.github.com>
2024-12-01 14:53:29 +0800

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.TextCore.Text;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    private void Start()
+    {
+        print("Hello world!");//--- программа выводит Привет мир!
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
